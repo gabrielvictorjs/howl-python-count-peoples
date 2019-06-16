375734fceb8967d263996e72d48f7777c0fa7600
Gabriel Victor <dester.gvpdv@gmail.com>
2019-06-16 16:41:10 -0300

    pronto

--- PeopleCounterMain.py
+++ PeopleCounterMain.py
@@ -16,41 +16,31 @@ def testIntersectionIn(y_rec, y_blueline):
 
 
 if __name__ == "__main__":
-    camera = cv2.VideoCapture("camerasuperior2.wmv")
+    camera = cv2.VideoCapture("principal.wmv")
 
     width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     firstFrame = None
 
-    # loop over the frames of the video
     while True:
-        # grab the current frame and initialize the occupied/unoccupied
-        # text
         (grabbed, frame) = camera.read()
         text = "Unoccupied"
 
-        # if the frame could not be grabbed, then we have reached the end
-        # of the video
         if not grabbed:
             break
 
-        # resize the frame, convert it to grayscale, and blur it
         frame = imutils.resize(frame, width=width)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-        # if the first frame is None, initialize it
         if firstFrame is None:
             firstFrame = gray
             continue
 
-        # compute the absolute difference between the current frame and
-        # first frame
         frameDelta = cv2.absdiff(firstFrame, gray)
         thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-        # dilate the thresholded image to fill in holes, then find contours
-        # on thresholded image
+
         thresh = cv2.dilate(thresh, None, iterations=2)
         cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
         # loop over the contours
@@ -64,8 +54,8 @@ if __name__ == "__main__":
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
             y_blueline = height // 2
+            x_blueline = width // 2
 
-            y_redline = y_blueline - 30
 
             cv2.line(frame, (0, y_blueline), (width, y_blueline), (250, 0, 1), 2)
             #cv2.line(frame, (0, y_redline), (width, y_redline), (0, 0, 255), 2)
@@ -78,15 +68,6 @@ if __name__ == "__main__":
 
             if (testIntersectionIn(y_rectangle, y_blueline)):
                 textIn += 1
-
-			# if (testIntersectionOut((x + x + w) // 2, (y + y + h) // 2)):
-			# 	textOut += 1
-
-            # draw the text and timestamp on the frame
-
-            # show the frame and record if the user presses a key
-            #cv2.imshow("Thresh", thresh)
-            #cv2.imshow("Frame Delta", frameDelta)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
